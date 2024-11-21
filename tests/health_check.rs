097fbd88f7866f3981e427ecd18b9fdec628cd9b
Ayush <shuklaayushanand@gmail.com>
2024-11-21 16:42:19 +0530

    first integration test

---
+++ tests/health_check.rs
@@ -0,0 +1,23 @@
+// use zero2prod::main;
+
+// #[test]
+// fn dummy_test(){
+//     main()
+// }
+
+#[tokio::test]
+async fn health_check_works() {
+    spawn_app().await.expect("Failed to spawn our app.");
+    let client = reqwest::Client::new();
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request.");
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+async fn spawn_app() -> Result<(), std::io::Error> {
+    // todo!()
+    zero2prod::run().await
+}
